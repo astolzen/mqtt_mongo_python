1648fbfb1c7ed252887e04fd383f2537ef744225
Andreas Stolzenberger <astolzen@redhat.com>
2015-04-29 09:31:31 +0200

    Added Topic selector in Flask App

--- flask/mongo_flask_read.py
+++ flask/mongo_flask_read.py
@@ -10,7 +10,7 @@
 
 from flask import Flask
 from flask.ext.pymongo import PyMongo
-from flask import render_template
+from flask import render_template,request,redirect
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -18,13 +18,18 @@ Bootstrap(app)
 app.config['MONGO_HOST'] = 'localhost'
 app.config['MONGO_PORT'] = 27017
 app.config['MONGO_DBNAME'] = 'test'
+topic = '#'
 mongo = PyMongo(app, config_prefix='MONGO')
 #mongo = PyMongo(app)
 
 @app.route('/')
 def home_page():
-    mqtt_messages = mongo.db.testcol01.find()
-    return render_template('mqtt.html',mqtt_messages=mqtt_messages)
+   topic = request.args.get('topic')
+   if not topic:
+      mqtt_messages = mongo.db.testcol01.find()
+   else:
+      mqtt_messages = mongo.db.testcol01.find({"topic":topic})
+   return render_template('mqtt.html',mqtt_messages=mqtt_messages,topic=topic)
 
 if __name__ == "__main__":
     app.run(debug=True)
